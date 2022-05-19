08d6853fd6607cfd8f011186cf4aee68689475ce
MikuAkita <miku.akita.ctc@gmail.com>
2022-05-19 11:21:38 +0900

    秋田_事業者６,事業者ページ名変更

--- BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage6.java
+++ BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage6.java
@@ -41,27 +41,17 @@ public class BusinessPage6 extends AppCompatActivity {
                     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                         int date = 0;
                         String message = year + "/" + (month + 1) + "/" + date;
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                        // Intent を生成
+                        // 第一引数はこの処理のContext
+                        // 第二引数に遷移先画面のSampleActivityを指定
+                        Intent newIntent = new Intent(btn.getContext(), ReservationDetails.class);
+
+                        // Intent にデータを保存
+                        newIntent.putExtra("KEY7", "value");// 新規画面表示実行
+                        startActivity(newIntent);
                     }
                 });
-
-        String[] platformVersion = {
-                "予約時間：12:30",
-                "来店人数：5人",
-                "内訳：アメリカ(2),タイ(1),韓国(1),サウジアラビア(1)",
-                "備考：ヴィーガン(1),ムスリム(1)",
-
-        };
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                getApplicationContext(),
-                android.R.layout.simple_list_item_1,    // Androidに組み込まれているレイアウト
-                platformVersion
-        );
-
-        ListView listView = findViewById(R.id.ReservationList);
-
-        listView.setAdapter(arrayAdapter);
     }
 
 }
