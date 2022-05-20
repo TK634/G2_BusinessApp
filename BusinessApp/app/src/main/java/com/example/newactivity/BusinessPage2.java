10e4ad1cbac56bfde8f37c041f400c6a4ccb2376
MikuAkita <miku.akita.ctc@gmail.com>
2022-05-20 17:00:25 +0900

    秋田_デザイン修正

--- BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage2.java
+++ BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage2.java
@@ -28,6 +28,20 @@ public class BusinessPage2 extends AppCompatActivity {
 
 
         };
+        Button btn = findViewById(R.id.ButtonToHome);
+            btn.setOnClickListener(v -> {
+            // Intent を生成
+            // 第一引数はこの処理のContext
+            // 第二引数に遷移先画面のSampleActivityを指定
+            Intent newIntent = new Intent(btn.getContext(), BusinessPage4.class);
+
+            // Intent にデータを保存
+            newIntent.putExtra("KEY7", "value");
+
+            // 新規画面表示実行
+            startActivity(newIntent);
+
+        });
 }
 
 
