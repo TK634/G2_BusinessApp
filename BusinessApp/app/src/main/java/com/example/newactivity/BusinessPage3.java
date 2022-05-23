25e9336812db54cdbc78715e4613107331a9abc7
MikuAkita <miku.akita.ctc@gmail.com>
2022-05-23 16:12:44 +0900

    秋田_ホームデザイン

--- BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage3.java
+++ BusinessApp/app/src/main/java/com/example/newactivity/BusinessPage3.java
@@ -3,6 +3,7 @@ package com.example.newactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,5 +29,21 @@ public class BusinessPage3 extends AppCompatActivity {
             startActivity(newIntent);
 
         });
+        //ホームボタン
+        ImageButton btn2 = findViewById(R.id.HomeButton);
+        btn2.setOnClickListener(v -> {
+            // Intent を生成
+            // 第一引数はこの処理のContext
+            // 第二引数に遷移先画面のSampleActivityを指定
+            Intent newIntent = new Intent(btn.getContext(), BusinessPage4.class);
+
+            // Intent にデータを保存
+            newIntent.putExtra("KEY7", "value");
+
+            // 新規画面表示実行
+            startActivity(newIntent);
+
+        });
+
     }
 }
